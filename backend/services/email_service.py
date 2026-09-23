99b7f7a4c8e17c1416f647d67b23f25c1ff6c2c5
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-09-23 10:04:44 +0530

    Update email_service.py

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -2,7 +2,7 @@ import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from backend.config import settings
+from config import settings
 
 logger = logging.getLogger(__name__)
 
